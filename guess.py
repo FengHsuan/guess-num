30283d11adef6a8c143bbe3bb618325eb550e0b7
Angel Feng <angelfeng14@gmail.com>
2023-02-07 23:57:03 +0800

    add count

--- guess.py
+++ guess.py
@@ -2,17 +2,22 @@
 # 讓使用者重複輸入數字去猜
 # 猜對的話 印出“終於猜對了”
 # 猜錯的話 要告訴他 比答案大還是小
+# 衍伸題
+# 顯示猜的次數
 
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜數字：')
 	num = int(num)
 	if num == r:
 		print('恭喜猜對了！')
+		print('這是你猜的第', count,'次')
 		break
 	elif num > r:
 		print('比答案大')
-	
 	elif num < r:
 		print('比答案小')
+	print('這是你猜的第', count,'次')
